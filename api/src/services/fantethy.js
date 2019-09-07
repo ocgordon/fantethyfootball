ba9923a3967f02d141e8253441ba50c9c8e8aae3
devonyarbrough <devonyarbrough@icloud.com>
2019-09-07 19:58:23 -0400

    updates

--- api/src/services/fantethy.js
+++ api/src/services/fantethy.js
@@ -1,8 +1,8 @@
-import { getContractWithSigner } from '../ethers';
+import { getContractWithSigner, getContract } from '../ethers';
 
 export const addPlayersToTeam = async (user, player) => {
   try {
-    const contract = await getContractWithSigner('0x627306090abaB3A6e1400e9345bC60c78a8BEf57');
+    const contract = await getContractWithSigner('0xdc3821270026617A3c712f04df9e891c925A1d42');
     const receipt = await contract.addPlayersToTeam(user, player);
     return receipt;
   } catch (e) {
@@ -13,7 +13,7 @@ export const addPlayersToTeam = async (user, player) => {
 
 export const getPlayersOnTeam = async user => {
   try {
-    const contract = await getContractWithSigner('0x627306090abab3a6e1400e9345bc60c78a8bef57');
+    const contract = await getContract();
     const footballTeam = await contract.getFootballTeam(user);
     return footballTeam;
   } catch (e) {
@@ -23,7 +23,7 @@ export const getPlayersOnTeam = async user => {
 
 export const assignPoints = async user => {
   try {
-    const contract = await getContractWithSigner('0x627306090abab3a6e1400e9345bc60c78a8bef57');
+    const contract = await getContractWithSigner('0xdc3821270026617A3c712f04df9e891c925A1d42');
     const pointsDistributed = await contract.distributePoints(user);
     return pointsDistributed;
   } catch (e) {
@@ -33,7 +33,7 @@ export const assignPoints = async user => {
 
 export const getPoints = async user => {
   try {
-    const contract = await getContractWithSigner('0x627306090abab3a6e1400e9345bc60c78a8bef57');
+    const contract = await getContractWithSigner('0xdc3821270026617A3c712f04df9e891c925A1d42');
     const pointsDistributed = await contract.userPoints(user);
     return pointsDistributed;
   } catch (e) {
