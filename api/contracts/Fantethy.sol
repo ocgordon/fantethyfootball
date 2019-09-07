8c32476e72b2dbc2c0d82b6bb33a4e7da4895dc7
devonyarbrough <devonyarbrough@icloud.com>
2019-09-07 11:00:59 -0400

    updates

--- api/contracts/Fantethy.sol
+++ api/contracts/Fantethy.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.11;
+pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract Fantethy {
@@ -11,22 +11,22 @@ contract Fantethy {
         0x821aEa9a577a9b44299B9c15c88cf3087F3b5544
     ];
 
-    mapping(address => string[11]) public footballPlayers;
+    mapping(address => string[]) public footballPlayers;
 
     function getAllUsers() public view returns(address[] memory) {
         return users;
     }
 
-    function addPlayersToTeam(address user, string[11] memory footballPlayerList) public returns(bool) {
-        for (uint i = 0; i < 11; i++) {
-            footballPlayers[user][i] = footballPlayerList[i];
-        }
+    function addPlayersToTeam(address userAccount, string memory footballPlayer) public returns(bool) {
+        //uint teamLength = footballPlayers[user].length;
+        footballPlayers[userAccount].push(footballPlayer);
         return true;
     }
 
     function getFootballTeam(address user) public view returns(string[] memory) {
-        string[] memory players = new string[](11);
-        for (uint i = 0; i < 11; i++) {
+        uint teamLength = footballPlayers[user].length;
+        string[] memory players = new string[](teamLength);
+        for (uint i = 0; i < teamLength; i++) {
             players[i] = footballPlayers[user][i];
         }
         return players;
