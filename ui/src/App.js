de34a0b4ad557e89f03f0acc018fea4c8e3989c4
ocgordon <ocgordon@github.com>
2019-09-06 23:31:56 -0400

    add react router

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import football from './football.png';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
+function Index() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h2>
+      {' '}
+      <img src={football} className="App-logo" alt="logo" />
+    </h2>
   );
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
