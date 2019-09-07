241bb97ef3d577050f374c95e5d91f0c38d00a4b
ocgordon <ocgordon@github.com>
2019-09-07 00:04:48 -0400

    start homepage

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import football from './football.png';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import HomepageLayout from './components/homepage';
 
 function Index() {
-  return (
-    <h2>
-      {' '}
-      <img src={football} className="App-logo" alt="logo" />
-    </h2>
-  );
+  return <h2>Home</h2>;
 }
 
 function About() {
@@ -22,28 +18,57 @@ function Users() {
 
 function AppRouter() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
-        </nav>
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
-      </div>
-    </Router>
+          <Route path="/" exact component={HomepageLayout} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
+    </div>
   );
 }
 
 export default AppRouter;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={football} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+{
+  /* <img src={football} className="App-logo" alt="logo" /> */
+}
+
+// export default App;
