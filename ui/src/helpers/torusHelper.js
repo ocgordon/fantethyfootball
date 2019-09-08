9fa0ca998aae946d9147d5024d69f77fd215035e
devonyarbrough <devonyarbrough@icloud.com>
2019-09-08 01:21:16 -0400

    adding Torus

---
+++ ui/src/helpers/torusHelper.js
@@ -0,0 +1,18 @@
+import Web3 from 'web3'
+import Torus from '@toruslabs/torus-embed';
+
+const web3Obj = {
+  web3: new Web3(),
+  setweb3: function(provider) {
+    const web3Inst = new Web3(provider)
+    web3Obj.web3 = web3Inst
+    sessionStorage.setItem('pageUsingTorus', true)
+  },
+  initialize: async function() {
+    const torus = new Torus()
+    await torus.init()
+    await torus.login()
+    web3Obj.setweb3(torus.provider)
+  }
+}
+export default web3Obj;
